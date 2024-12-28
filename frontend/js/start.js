6fdec0c252e1a54470c2e91e2c8b625cbf55de91
coffeekosatka <viikkuullyy@gmail.com>
2024-12-28 16:00:07 +0300

    add frontend: modal for reg and modal for forgot

--- frontend/js/start.js
+++ frontend/js/start.js
@@ -1,11 +1,20 @@
 const cross = document.getElementById('cross');
+const crossReg = document.getElementById('crossReg');
+const crossFog = document.getElementById('crossFog');
 const modalWindow = document.getElementById('modalwindow');
 const modalWindow2 = document.getElementById('modalwindow2');
+const modalWindowReg = document.getElementById('modalwindowReg');
+const modalWindow3 = document.getElementById('modalwindow3');
+const modalWindowFog = document.getElementById('modalwindowFog');
+const modalWindow4 = document.getElementById('modalwindow4');
 const overlay = document.getElementById('modalopened');
 const slogan = document.getElementById('slogan');
 const startBtn = document.getElementById('startbtn');
+const preregBtn = document.getElementById('preregbtn');
+const forBtn = document.getElementById('forgot');
 
 function closeModal(){
+    modalWindow.style.display = 'none';
     modalWindow.classList.add('closed');
     modalWindow2.classList.add('closed');
     cross.classList.add('closed');
@@ -16,6 +25,7 @@ function closeModal(){
 }
 cross.addEventListener('click', closeModal);
 function openModal(){
+    modalWindow.style.display = 'flex';
     modalWindow.classList.remove('closed');
     modalWindow2.classList.remove('closed');
     cross.classList.remove('closed');
@@ -23,15 +33,57 @@ function openModal(){
     slogan.classList.add('sloganclosed');
     startBtn.classList.add('sloganclosed');
 }
+function closeModalReg(){
+    modalWindowReg.style.display = 'none';
+    modalWindowReg.classList.add('closed');
+    modalWindow3.classList.add('closed');
+    crossReg.classList.add('closed');
+    overlay.classList.remove('modalopened');
+    slogan.classList.remove('sloganclosed');
+    startBtn.classList.remove('sloganclosed');
+
+}
+crossReg.addEventListener('click', closeModalReg);
+function openModalReg(){
+    closeModal();
+    modalWindowReg.style.display = 'flex';
+    modalWindowReg.classList.remove('closed');
+    modalWindow3.classList.remove('closed');
+    crossReg.classList.remove('closed');
+    overlay.classList.add('modalopened');
+    slogan.classList.add('sloganclosed');
+    startBtn.classList.add('sloganclosed');
+    
+}
+preregBtn.addEventListener('click', openModalReg);
+
+function openModalFog(){
+    closeModal();
+    modalWindowFog.style.display = 'flex';
+    modalWindowFog.classList.remove('closed');
+    modalWindow4.classList.remove('closed');
+    crossFog.classList.remove('closed');
+    overlay.classList.add('modalopened');
+    slogan.classList.add('sloganclosed');
+    startBtn.classList.add('sloganclosed');
+}
+function closeModalFog(){
+    modalWindowFog.style.display = 'none';
+    modalWindowFog.classList.add('closed');
+    modalWindow4.classList.add('closed');
+    crossFog.classList.add('closed');
+    overlay.classList.remove('modalopened');
+    slogan.classList.remove('sloganclosed');
+    startBtn.classList.remove('sloganclosed');
+}
+forBtn.addEventListener('click', openModalFog);
+crossFog.addEventListener('click', closeModalFog);
+
+
+
+
 
 startBtn.addEventListener('click', openModal);
-
-
-
-
-
-
-
 
 // Получаем элементы формы
 const form = document.getElementById('login-form');
